e535d20b83139448f510589d723bf4c1a24926c4
HeroBlackInk <drv1@outlook.com>
2021-11-29 14:11:13 +0800

    增加了绿书宝例句提取插件

---
+++ fastWQ/sentenceFromXDFlvBaoShu.py
@@ -0,0 +1,42 @@
+#-*- coding:utf-8 -*-
+import random
+from ..base import *
+import re
+from urllib.parse import unquote
+
+
+
+# import the main window object (mw) from aqt
+from aqt import mw
+# import the "show info" tool from utils.py
+from aqt.utils import showInfo
+# import all of the Qt GUI library
+from aqt.qt import *
+
+
+@register(u'绿宝书中提取例句')
+class sentencesFromXDFLvBaoShu(WebService):
+
+    def __init__(self):
+        super(sentencesFromXDFLvBaoShu, self).__init__()
+
+    def _get_from_api(self):
+        url_string  = self.quote_word
+        string = unquote(url_string, 'utf-8')
+
+        result = {
+            'sentence': u'',
+        }
+        
+        #匹配 例字之后的句子
+        pattern = "(?<=\u4f8b)[\s\S]*\."  
+        result['sentence'] = re.search( r'(?<=\u4f8b)\s[\s\S]*\.', string, re.M|re.I).group(0)
+
+
+
+
+        return self.cache_this(result)
+
+    @export([u'绿宝书中提取例句', u'sentences From XDF Lv Bao Shu'])
+    def fld_definate(self):
+        return self._get_field('sentence')
